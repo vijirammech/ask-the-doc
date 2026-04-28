d06332be7a9efc9b8f0130e365fa9373a10e9e83
vijirammech <vijirammech@gmail.com>
2026-04-27 20:52:59 -0500

    Loosen dependency pins and add pysqlite3 shim for Streamlit Cloud

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__("pysqlite3")
+import sys
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
 import streamlit as st
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
